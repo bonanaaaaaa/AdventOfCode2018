73beba82a68edbe1684f99be57883c48e5a3179b
Pittawat Mongkonrat <pittawatm@gmail.com>
2018-12-05 00:41:38 +0700

    :hammer: Clean up elixir code

--- day04/02/02.ex
+++ day04/02/02.ex
@@ -39,8 +39,6 @@ ans =
       guards: %{}
     },
     fn event, m ->
-      # IO.inspect(m)
-
       cond do
         Regex.match?(guard_id_regex, event.event) ->
           id = Regex.named_captures(guard_id_regex, event.event) |> Map.get("id")
@@ -58,11 +56,10 @@ ans =
           Map.put(m, :time_before, event.datetime)
 
         event.event == "wakes up" ->
-          diff = Float.floor(DateTime.diff(event.datetime, m.time_before) / 60) |> Kernel.trunc()
-
-          diff = diff - 1
-
-          # IO.puts("#{m.current_id} => #{diff}")
+          diff =
+            Float.floor(DateTime.diff(event.datetime, m.time_before) / 60)
+            |> Kernel.trunc()
+            |> Kernel.-(1)
 
           m1 = m.time_before.minute
 
@@ -75,14 +72,14 @@ ans =
 
           diff_m = m2 - m1
 
-          if diff != diff_m do
-            diff_m2 = m2
-            diff_m1 = 60 - m1
+          guards_map =
+            if diff != diff_m do
+              diff_m2 = m2
+              diff_m1 = 60 - m1
 
-            core = diff - diff_m2 - diff_m1
-            round = Float.floor(core / 60) |> Kernel.trunc()
+              core = diff - diff_m2 - diff_m1
+              round = Float.floor(core / 60) |> Kernel.trunc()
 
-            guards_map =
               Enum.reduce(
                 [
                   Enum.reduce(0..59, %{}, fn m, mm ->
@@ -98,18 +95,13 @@ ans =
                 Map.get(m.guards, m.current_id),
                 fn m, acc -> Map.merge(m, acc, fn _k, v1, v2 -> v1 + v2 end) end
               )
-
-            # IO.inspect(guards_map)
-
-            Map.put(m, :guards, Map.put(m.guards, m.current_id, guards_map))
-          else
-            guards_map =
+            else
               Enum.reduce(m1..m2, Map.get(m.guards, m.current_id), fn m, mm ->
                 Map.update(mm, m, 1, &(&1 + 1))
               end)
+            end
 
-            Map.put(m, :guards, Map.put(m.guards, m.current_id, guards_map))
-          end
+          Map.put(m, :guards, Map.put(m.guards, m.current_id, guards_map))
       end
     end
   )
@@ -120,7 +112,6 @@ ans =
   Enum.reduce(Map.keys(ans.guards), %{}, fn gk, acc ->
     max_list =
       Map.get(ans.guards, gk)
-      # |> IO.inspect()
       |> Map.to_list()
 
     if Enum.empty?(max_list) do
