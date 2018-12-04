9ac97a3784d1738c8362c1e8eac408f528fc51ad
Pittawat Mongkonrat <pittawatm@gmail.com>
2018-12-05 00:44:48 +0700

    :laughing: Uncomment code

---
+++ day03/01/01_0.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type position struct {
+	x1 int
+	x2 int
+	y1 int
+	y2 int
+}
+
+var mem = make(map[string]bool)
+
+func intersectArea(rec1, rec2 position) int {
+	area := 0
+	for x := rec1.x1; x <= rec1.x2; x++ {
+		for y := rec1.y1; y <= rec1.y2; y++ {
+			if rec2.x1 <= x && x <= rec2.x2 && rec2.y1 <= y && y <= rec2.y2 {
+				k := strconv.Itoa(x) + "," + strconv.Itoa(y)
+				if _, ok := mem[k]; !ok {
+					area++
+				}
+				mem[k] = true
+			}
+		}
+	}
+	return area
+}
+
+func main() {
+	file, _ := os.Open("input.txt")
+	// file, _ := os.Open("sample.txt")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var input []string
+
+	var arr []position
+
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	for i := 0; i < len(input); i++ {
+		r := strings.Split(input[i], "@ ")
+		ps := strings.Split(r[1], ": ")
+		xy := strings.Split(ps[0], ",")
+		len := strings.Split(ps[1], "x")
+		x, _ := strconv.Atoi(xy[0])
+		y, _ := strconv.Atoi(xy[1])
+		lenX, _ := strconv.Atoi(len[0])
+		lenY, _ := strconv.Atoi(len[1])
+
+		pos := position{
+			x1: (x + 1),
+			x2: (x + lenX),
+			y1: (y + 1),
+			y2: (y + lenY),
+		}
+
+		arr = append(arr, pos)
+	}
+
+	ans := 0
+
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			ans += intersectArea(arr[i], arr[j])
+		}
+	}
+
+	fmt.Println(ans)
+}
--- day03/01/01_2.go
+++ day03/01/01_2.go
@@ -1,116 +1,116 @@
-// package main
+package main
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"os"
-// 	"strconv"
-// 	"strings"
-// )
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-// type position struct {
-// 	x1 int
-// 	x2 int
-// 	y1 int
-// 	y2 int
-// }
+type position struct {
+	x1 int
+	x2 int
+	y1 int
+	y2 int
+}
 
-// func toMap(pos position) map[string]int {
-// 	m := make(map[string]int)
-// 	for x := pos.x1; x <= pos.x2; x++ {
-// 		for y := pos.y1; y <= pos.y2; y++ {
-// 			k := strconv.Itoa(x) + "," + strconv.Itoa(y)
-// 			m[k] = 1
-// 		}
-// 	}
-// 	return m
-// }
+func toMap(pos position) map[string]int {
+	m := make(map[string]int)
+	for x := pos.x1; x <= pos.x2; x++ {
+		for y := pos.y1; y <= pos.y2; y++ {
+			k := strconv.Itoa(x) + "," + strconv.Itoa(y)
+			m[k] = 1
+		}
+	}
+	return m
+}
 
-// func divide(list []position, ch chan map[string]int) {
-// 	if len(list) == 0 {
-// 		ch <- make(map[string]int)
-// 		return
-// 	}
+func divide(list []position, ch chan map[string]int) {
+	if len(list) == 0 {
+		ch <- make(map[string]int)
+		return
+	}
 
-// 	if len(list) == 1 {
-// 		ch <- toMap(list[0])
-// 		return
-// 	}
+	if len(list) == 1 {
+		ch <- toMap(list[0])
+		return
+	}
 
-// 	halfSize := int(len(list) / 2)
-// 	leftChan := make(chan map[string]int, 1)
-// 	rightChan := make(chan map[string]int, 1)
-// 	go divide(list[:halfSize], leftChan)
-// 	go divide(list[halfSize:], rightChan)
+	halfSize := int(len(list) / 2)
+	leftChan := make(chan map[string]int, 1)
+	rightChan := make(chan map[string]int, 1)
+	go divide(list[:halfSize], leftChan)
+	go divide(list[halfSize:], rightChan)
 
-// 	listA := <-leftChan
-// 	listB := <-rightChan
+	listA := <-leftChan
+	listB := <-rightChan
 
-// 	mergedMap := map[string]int{}
+	mergedMap := map[string]int{}
 
-// 	for k, v := range listA {
-// 		mergedMap[k] = v
-// 	}
+	for k, v := range listA {
+		mergedMap[k] = v
+	}
 
-// 	for k, v := range listB {
-// 		if vv, ok := mergedMap[k]; ok {
-// 			mergedMap[k] = vv + v
-// 		} else {
-// 			mergedMap[k] = v
-// 		}
-// 	}
+	for k, v := range listB {
+		if vv, ok := mergedMap[k]; ok {
+			mergedMap[k] = vv + v
+		} else {
+			mergedMap[k] = v
+		}
+	}
 
-// 	ch <- mergedMap
-// }
+	ch <- mergedMap
+}
 
-// func main() {
-// 	file, _ := os.Open("input.txt")
-// 	// file, _ := os.Open("sample.txt")
+func main() {
+	file, _ := os.Open("input.txt")
+	// file, _ := os.Open("sample.txt")
 
-// 	defer file.Close()
+	defer file.Close()
 
-// 	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-// 	var input []string
+	var input []string
 
-// 	var arr []position
+	var arr []position
 
-// 	for scanner.Scan() {
-// 		input = append(input, scanner.Text())
-// 	}
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
 
-// 	for i := 0; i < len(input); i++ {
-// 		r := strings.Split(input[i], "@ ")
-// 		ps := strings.Split(r[1], ": ")
-// 		xy := strings.Split(ps[0], ",")
-// 		len := strings.Split(ps[1], "x")
-// 		x, _ := strconv.Atoi(xy[0])
-// 		y, _ := strconv.Atoi(xy[1])
-// 		lenX, _ := strconv.Atoi(len[0])
-// 		lenY, _ := strconv.Atoi(len[1])
+	for i := 0; i < len(input); i++ {
+		r := strings.Split(input[i], "@ ")
+		ps := strings.Split(r[1], ": ")
+		xy := strings.Split(ps[0], ",")
+		len := strings.Split(ps[1], "x")
+		x, _ := strconv.Atoi(xy[0])
+		y, _ := strconv.Atoi(xy[1])
+		lenX, _ := strconv.Atoi(len[0])
+		lenY, _ := strconv.Atoi(len[1])
 
-// 		o := position{
-// 			x1: (x + 1),
-// 			x2: (x + lenX),
-// 			y1: (y + 1),
-// 			y2: (y + lenY),
-// 		}
+		o := position{
+			x1: (x + 1),
+			x2: (x + lenX),
+			y1: (y + 1),
+			y2: (y + lenY),
+		}
 
-// 		arr = append(arr, o)
-// 	}
+		arr = append(arr, o)
+	}
 
-// 	ch := make(chan map[string]int)
+	ch := make(chan map[string]int)
 
-// 	go divide(arr, ch)
+	go divide(arr, ch)
 
-// 	positionMap := <-ch
+	positionMap := <-ch
 
-// 	ans := 0
-// 	for _, v := range positionMap {
-// 		if v >= 2 {
-// 			ans++
-// 		}
-// 	}
+	ans := 0
+	for _, v := range positionMap {
+		if v >= 2 {
+			ans++
+		}
+	}
 
-// 	fmt.Println(ans)
-// }
+	fmt.Println(ans)
+}
